3a5d980dde1021af6b0a7d32fd2ef4f0a40a86aa
Jac Casey <JacCas2014@users.noreply.github.com>
2016-02-13 17:45:01 +0000

    Jac - changed tests to use midje and future facted all tests

--- beer_song_test.clj
+++ beer_song_test.clj
@@ -1,7 +1,7 @@
 (ns beer-song-test
-  (:require [clojure.test :refer :all]))
-
-(load-file "beer_song.clj")
+  (:require [clojure.test :refer :all]
+            [beer-song :refer :all]
+            [midje.sweet :refer :all]))
 
 (def verse-8 "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n")
 (def verse-2 "2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n")
@@ -11,14 +11,19 @@
 (def song-8-6 "8 bottles of beer on the wall, 8 bottles of beer.\nTake one down and pass it around, 7 bottles of beer on the wall.\n\n7 bottles of beer on the wall, 7 bottles of beer.\nTake one down and pass it around, 6 bottles of beer on the wall.\n\n6 bottles of beer on the wall, 6 bottles of beer.\nTake one down and pass it around, 5 bottles of beer on the wall.\n")
 (def song-3-0 "3 bottles of beer on the wall, 3 bottles of beer.\nTake one down and pass it around, 2 bottles of beer on the wall.\n\n2 bottles of beer on the wall, 2 bottles of beer.\nTake one down and pass it around, 1 bottle of beer on the wall.\n\n1 bottle of beer on the wall, 1 bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n\nNo more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n")
 
-(deftest test-verse
-  (is (= verse-8 (beer-song/verse 8)))
-  (is (= verse-2 (beer-song/verse 2)))
-  (is (= verse-1 (beer-song/verse 1)))
-  (is (= verse-0 (beer-song/verse 0))))
+(facts "verse function"
+       (fact "returns verse 8 when given number 8"
+             (beer-song/verse 8) => verse-8)
+       (fact "returns verse 2 when given number 2"
+             (beer-song/verse 2) => verse-2)
+       (fact "returns verse 1 when given number 1"
+             (beer-song/verse 1) => verse-1)
+       (fact "returns verse 0 when given number 0"
+             (beer-song/verse 0) => verse-0))
 
-(deftest test-song
-  (is (= song-8-6 (beer-song/sing 8 6)))
-  (is (= song-3-0 (beer-song/sing 3))))
+(facts "sing function"
+       (fact " returns song 8 to 6 when given number 8 and 6"
+             (beer-song/sing 8 6) => song-8-6)
+       (fact "returns song 3 to 0 when given number 3 only"
+             (beer-song/sing 3) => song-3-0))
 
-(run-tests)
