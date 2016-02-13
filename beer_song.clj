1ce5e3d5a4fb1b7e47a37b29ed078289ae19d346
Jac Casey <JacCas2014@users.noreply.github.com>
2016-02-13 18:54:10 +0000

    Jac - changed verse function to use a case statement as more readable.

--- beer_song.clj
+++ beer_song.clj
@@ -4,20 +4,11 @@
   (when (> number 1)
     "s"))
 
-(defn it-or-one [number]
-  (if (> number 1)
-    "one"
-    "it"))
-
-(defn bottles-of-beer-left [number]
-  (if (> number 1)
-    (str (- number 1) " bottle" (pluralise (- number 1)))
-    "no more bottles"))
-
 (defn verse [number]
-  (if (= number 0)
-    "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"
-    (str number " bottle" (pluralise number) " of beer on the wall, " number " bottle" (pluralise number) " of beer.\nTake " (it-or-one number) " down and pass it around, " (bottles-of-beer-left number) " of beer on the wall.\n")))
+  (cond
+    (> number 1) (str number " bottle" (pluralise number) " of beer on the wall, " number " bottle" (pluralise number) " of beer.\nTake " (it-or-one number) " down and pass it around, " (bottles-of-beer-left number) " of beer on the wall.\n")
+    (= number 1) (str number " bottle of beer on the wall, " number " bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n")
+    :else "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"))
 
 (defn sing
   ([number])
