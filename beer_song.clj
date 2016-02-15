4dab98029af7a06e6c6e6ad74ff0d78a32d2e063
Jac Casey <JacCas2014@users.noreply.github.com>
2016-02-15 09:16:48 +0000

    Jac - simplified sing function by using map and string.join.

--- beer_song.clj
+++ beer_song.clj
@@ -1,4 +1,5 @@
-(ns beer-song)
+(ns beer-song
+  (:require [clojure.string :as str]))
 
 (defn pluralise [number]
   (when (> number 1)
@@ -10,19 +11,8 @@
     (= number 1) (str number " bottle of beer on the wall, " number " bottle of beer.\nTake it down and pass it around, no more bottles of beer on the wall.\n")
     :else "No more bottles of beer on the wall, no more bottles of beer.\nGo to the store and buy some more, 99 bottles of beer on the wall.\n"))
 
-(defn add-current-verse-to-result [result coll]
-  (let [result-and-verse (str result (verse (first coll)))]
-    (if (= 1 (count coll))
-      result-and-verse
-      (str result-and-verse "\n"))))
-
-(defn create-verses [coll result]
-  (if (= 0 (count coll))
-    result
-    (recur (rest coll) (add-current-verse-to-result result coll))))
-
 (defn sing
   ([startNumber]
    (sing startNumber 0))
   ([startNumber endNumber]
-   (create-verses (range startNumber (- endNumber 1) -1) "")))
+   (str/join "\n" (map #(verse %) (range startNumber (- endNumber 1) -1)))))
